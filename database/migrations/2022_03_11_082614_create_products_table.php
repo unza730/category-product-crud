5295823dc0a26a8f606e3b31afff4567f80f5330
unza730122 <sarisha505@gmail.com>
2022-03-12 08:03:06 +0500

    first commit

---
+++ database/migrations/2022_03_11_082614_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->bigInteger('category_id');
+            $table->bigInteger('price');
+            $table->bigInteger('discount_percentage');
+            $table->binary('feature_image');
+            $table->binary('gallery');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->binary('meta_image');
+             $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
